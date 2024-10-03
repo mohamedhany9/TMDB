0609bd4f05896086515acef261adc7b64c3bf99b
ADMIN-PC <maohmedh@gmail.com>
2024-10-03 15:57:35 +0200

    update

--- lib/presentation/screens/person_image_screen.dart
+++ lib/presentation/screens/person_image_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
